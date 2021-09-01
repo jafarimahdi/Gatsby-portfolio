2490520547e4340400dc10345d336564a70bfe9b
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-09-01 18:01:37 +0200

    add the buttons

--- src/mock/data.js
+++ src/mock/data.js
@@ -68,3 +68,8 @@ export const footerData = {
     },
   ],
 };
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};

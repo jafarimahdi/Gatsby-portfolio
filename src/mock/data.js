9ee8367364431a98f76b3a06d23f05c1e5551d85
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-05-12 11:02:44 +0200

    update details

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,10 +18,13 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'I have a junior level of Problem-Solving, Data Structure, and database management Knowledge, while I learned to pay attention to detail and be open to learn new Tech and Design attitudes',
-  paragraphTwo: ' I have experience using HTML5, CSS3, Javascript, Typescript, Bootstrap, SCSS and MySql, Git, GitHub, Trello and had more Focus to learn creating efficient and user-friendly interfaces for websites and web applications; also some experience on architectural design area.',
-  paragraphThree: 'Also hands-on: React.js, Next.js, Rest API and Node.js, Python, while working with multiple Operating Systems: Mac, Windows, Linux: { Ubuntu, Mint }.',
-  // resume: './jafari-Mahdi.pdf', 
+  paragraphOne:
+    'I have a junior level of Problem-Solving, Data Structure, and database management Knowledge, while I learned to pay attention to detail and be open to learn new Tech and Design attitudes',
+  paragraphTwo:
+    ' I have experience using HTML5, CSS3, Javascript, Typescript, Bootstrap, SCSS and MySql, R & Shinnay, Git, GitHub, Trello and had more Focus to learn creating efficient and user-friendly interfaces for websites and web applications; also some experience on architectural design area.',
+  paragraphThree:
+    'Also hands-on: React.js, Next.js, Rest API and Node.js, Python, while working with multiple Operating Systems: Mac, Windows, Linux: { Ubuntu, Mint }.',
+  // resume: './jafari-Mahdi.pdf',
 };
 
 // PROJECTS DATA
@@ -31,7 +34,8 @@ export const projectsData = [
     img: 'kalunba.png',
     title: 'Kalunba Non Profit',
     info: 'This website as made it for an NGO, they located in Budapest and working with refugees and migrants. Dependencies been used in this project:',
-    info2: ' react-dom, react-hook-form, lodash-es, hookForm/resolver, aos( scroll animation), Emailjs-com, yup(Form validation), Bootstrap',
+    info2:
+      ' react-dom, react-hook-form, lodash-es, hookForm/resolver, aos( scroll animation), Emailjs-com, yup(Form validation), Bootstrap',
     url: 'https://kalunba.netlify.app/',
     repo: 'https://github.com/jafarimahdi/ReactJs/tree/main/kalunba', // if no repo, the button will not show up
   },
@@ -40,7 +44,14 @@ export const projectsData = [
     img: 'irancukrazda.png',
     title: 'Iran Cukrazda',
     info: `This website created for the a persian pastry, located in Budapest. Dependencies been used in this project:`,
-    info2: ['react-router-dom, ',' node-sass (scss), ',' react-geocode, ',' react-google-autocomplete, ',' react-google-maps, ', ' react-player'],
+    info2: [
+      'react-router-dom, ',
+      ' node-sass (scss), ',
+      ' react-geocode, ',
+      ' react-google-autocomplete, ',
+      ' react-google-maps, ',
+      ' react-player',
+    ],
     url: 'https://irancukrazda.netlify.app/',
     repo: 'https://github.com/jafarimahdi/ReactJs/tree/main/irancukrazda', // if no repo, the button will not show up
   },
@@ -49,7 +60,8 @@ export const projectsData = [
     img: 'adventure.png',
     title: 'Crypto Tracker',
     info: 'A simple R-Shinyapps for Tracking the Crypto Price from CoinGecko API V3',
-    info2: 'Library: Shiny, ggplot2, data.table, jsonlite, rsconnect, plotly, shinythemes, shinydashboard, MASS',
+    info2:
+      'Library: Shiny, ggplot2, data.table, jsonlite, rsconnect, plotly, shinythemes, shinydashboard, MASS',
     url: 'https://jafari-mahdi.shinyapps.io/Crypto-tracker/',
     repo: 'https://github.com/jafarimahdi/CEU-Traning/blob/main/R/final_Project/Crypto_tracker/app.R', // if no repo, the button will not show up
   },
